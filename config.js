541aa52bb10779effc0383d9fc0c3557967b6150
3s3cube-star <shinhogle@gmail.com>
2026-04-21 12:50:47 +0900

    GAS URL更新 v5

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // GAS の URL のみここで管理（フォーム設定はスプレッドシートで変更）
 const CONFIG = {
-  gasUrl: "https://script.google.com/macros/s/AKfycbyeU1y4lZzzeGiYmZ-Ssxz5g1KdcCE6Fg06GTvKy6B1pTULZwl66pejFuN06UwFsbgU/exec"
+  gasUrl: "https://script.google.com/macros/s/AKfycbyYZkYECr55N2b-WzQxBcOk_oiJ4C4CHiaXlY0VtELjlHMtF5S2PM7RogahRzDwJBno/exec"
 };
